3fa42a527ef40334eb31415147577d9c7ade3c6d
rajvi04 <156743343+rajvi04@users.noreply.github.com>
2026-03-03 16:30:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,12 @@ app.post("/submit", upload.fields([
 });
 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
 
